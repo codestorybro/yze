6ab5c9e40102a6bfa5f1193f137d2aeb6b1706b3
Marcin Kołodziej <mar.kolodziej@icloud.com>
2025-09-16 14:03:36 +0200

    feat: simplify bottomBar

--- src/components/TabBar/TabBarButton.tsx
+++ src/components/TabBar/TabBarButton.tsx
@@ -1,12 +1,6 @@
-import { ComponentProps, useEffect } from "react"
+import { ComponentProps } from "react"
 import { ViewStyle } from "react-native"
 import { PlatformPressable } from "@react-navigation/elements"
-import Animated, {
-  interpolate,
-  useAnimatedStyle,
-  useSharedValue,
-  withSpring,
-} from "react-native-reanimated"
 
 import { useAppTheme } from "@/theme/context"
 import { ThemedStyle } from "@/theme/types"
@@ -23,29 +17,16 @@ type TabBarButtonProps = Omit<ComponentProps<typeof PlatformPressable>, "childre
 
 export function TabBarButton({ routeName, color, label, isFocused, ...props }: TabBarButtonProps) {
   const { themed } = useAppTheme()
-  const scale = useSharedValue(0)
-
-  useEffect(() => {
-    scale.value = withSpring(isFocused ? 1 : 0, {
-      duration: 350,
-    })
-  }, [scale, isFocused])
-
-  const animatedIconStyle = useAnimatedStyle(() => {
-    const scaleValue = interpolate(scale.value, [0, 1], [1, 1.3])
-
-    return { transform: [{ scale: scaleValue }] }
-  })
 
   return (
     <PlatformPressable pressOpacity={1} style={themed($tabBarItem)} {...props}>
-      <Animated.View style={animatedIconStyle}>
-        <SvgIcon
-          pathData={SvgIconPaths[routeName as keyof typeof SvgIconPaths]}
-          color={color}
-          size={24}
-        />
-      </Animated.View>
+      <SvgIcon
+        pathData={
+          SvgIconPaths[`${routeName}${isFocused ? "_full" : ""}` as keyof typeof SvgIconPaths]
+        }
+        color={color}
+        size={25}
+      />
     </PlatformPressable>
   )
 }
@@ -54,6 +35,6 @@ const $tabBarItem: ThemedStyle<ViewStyle> = ({ spacing }) => ({
   flex: 1,
   justifyContent: "center",
   alignItems: "center",
-  paddingTop: spacing.lg,
+  paddingTop: spacing.md,
   paddingBottom: spacing.xxxs,
 })
