9376b7b48c19001d6fdd14786767e5ed25201b99
Marcin Kołodziej <mar.kolodziej@icloud.com>
2025-10-24 10:43:01 +0200

    fix: fix transition blinking between screens

--- src/app/(app)/(tabs)/search/_layout.tsx
+++ src/app/(app)/(tabs)/search/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router"
 
 export default function SearchLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <Stack screenOptions={{ headerShown: false, animation: "none" }}>
       <Stack.Screen name="index" />
     </Stack>
   )
